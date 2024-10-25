9654dec58a436d718800685161081d5ce02c27a8
Ksuvec <kec@umbraco.dk>
2024-10-25 10:22:13 +0200

    Added custom code

---
+++ src/UmbracoProject.Code/GithubHelper.cs
@@ -0,0 +1,13 @@
+﻿using Octokit;
+
+namespace UmbracoProject.Code
+{
+    public class GithubHelper
+    {
+        public static IReadOnlyList<Repository> GetRepositories(string organization)
+        {
+            var client = new GitHubClient(new ProductHeaderValue("my-cool-app"));
+            return client.Repository.GetAllForOrg(organization).Result;
+        }
+    }
+}
